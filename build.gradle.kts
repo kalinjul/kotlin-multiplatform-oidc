3537d9fb34eb4d8699d5dbfebb8e36057cc431b7
Julian Kalinowski <julakali@gmail.com>
2023-11-27 08:04:01 +0100

    ci: select xcode version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,9 @@ nexusPublishing {
         sonatype {
             nexusUrl.set(URI("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(URI("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username = getLocalProperty("OSSRH_USERNAME") ?: System.getenv("OSSRH_USERNAME")
-            password = getLocalProperty("OSSRH_PASSWORD") ?: System.getenv("OSSRH_PASSWORD")
-            stagingProfileId = getLocalProperty("SONATYPE_STAGING_PROFILE_ID") ?: System.getenv("SONATYPE_STAGING_PROFILE_ID")
+            username.set(getLocalProperty("OSSRH_USERNAME") ?: System.getenv("OSSRH_USERNAME"))
+            password.set(getLocalProperty("OSSRH_PASSWORD") ?: System.getenv("OSSRH_PASSWORD"))
+            stagingProfileId.set(getLocalProperty("SONATYPE_STAGING_PROFILE_ID") ?: System.getenv("SONATYPE_STAGING_PROFILE_ID"))
         }
     }
 }
