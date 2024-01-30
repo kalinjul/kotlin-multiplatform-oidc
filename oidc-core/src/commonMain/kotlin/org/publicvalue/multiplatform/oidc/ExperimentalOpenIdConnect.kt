08d76bf3b4004a96b47a42e51a1acb764cf469dc
Julian Kalinowski <julakali@gmail.com>
2024-01-30 07:45:02 +0100

    fix: experimental annotation produces warning now

--- oidc-core/src/commonMain/kotlin/org/publicvalue/multiplatform/oidc/ExperimentalOpenIdConnect.kt
+++ oidc-core/src/commonMain/kotlin/org/publicvalue/multiplatform/oidc/ExperimentalOpenIdConnect.kt
@@ -1,6 +1,6 @@
 package org.publicvalue.multiplatform.oidc
 
-@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
+@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.", level = RequiresOptIn.Level.WARNING)
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class ExperimentalOpenIdConnect
