3fede1bf6ca425e84b0e5fa105d615046b6e4019
Julian Kalinowski <julakali@gmail.com>
2024-02-19 19:26:10 +0100

    fix: remove incompatible kotlin compiler version in desktop preview

--- build-logic/convention/src/main/kotlin/org/publicvalue/convention/ComposeMultiplatformConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/org/publicvalue/convention/ComposeMultiplatformConventionPlugin.kt
@@ -1,12 +1,9 @@
 package org.publicvalue.convention
 
-import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.compose.ComposeExtension
-import org.jetbrains.compose.ComposePlugin
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class ComposeMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
@@ -18,37 +15,5 @@ class ComposeMultiplatformConventionPlugin : Plugin<Project> {
 fun Project.configureCompose() {
     with(extensions.getByType<ComposeExtension>()) {
         kotlinCompilerPlugin.set(libs.versions.composeCompiler.get())
-    }
-}
-
-fun Project.configureComposePreview() {
-    val compose = dependencies.extensions.getByType<ComposePlugin.Dependencies>()
-    with(extensions.getByType<KotlinMultiplatformExtension>()) {
-        with(sourceSets) {
-            val jvmMain = getByName("jvmMain")
-            val androidMain = getByName("androidMain")
-
-            with(androidMain) {
-                dependencies {
-                }
-            }
-            with(jvmMain) {
-                dependencies {
-                    implementation(compose.preview)
-                }
-            }
-        }
-    }
-
-    with(dependencies) {
-        add("debugImplementation", compose.uiTooling)
-        add("debugImplementation", compose.preview)
-    }
-
-    with(extensions.getByType<BaseExtension>()) {
-        buildFeatures.compose = true
-        composeOptions {
-            kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
-        }
     }
 }
