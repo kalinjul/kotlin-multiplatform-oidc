3d8e3ae66a44f413d0b9ef9b5283b172aef80210
Julian Kalinowski <julakali@gmail.com>
2024-05-23 09:07:38 +0200

    fix: workaround for xcode 15.3/15.4 appstore release

--- oidc-appsupport/build.gradle.kts
+++ oidc-appsupport/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
 import org.jetbrains.kotlin.gradle.plugin.mpp.Framework
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.publicvalue.convention.config.configureIosTargets
+import java.nio.file.Files
+import java.util.stream.Collectors.toList
+import kotlin.io.path.name
 
 plugins {
     id("org.publicvalue.convention.android.library")
@@ -14,6 +18,47 @@ description = "Kotlin Multiplatform OIDC appsupport library for Android/iOS"
 multiplatformSwiftPackage {
     packageName("OpenIdConnectClient")
     zipFileName("OpenIdConnectClient")
+}
+
+// workaround for https://forums.developer.apple.com/forums/thread/748177, remove once apple fixed it
+val fixTask = tasks.create("fixFrameworkPlist") {
+    val fixTask = this
+    group = "multiplatform-swift-package"
+    afterEvaluate {
+        val createFrameworkTask = tasks.named("createXCFramework").get()
+        val deps = createFrameworkTask.taskDependencies.getDependencies(createFrameworkTask)
+        fixTask.dependsOn(deps)
+    }
+    doFirst {
+        val createFrameworkTask = tasks.named("createXCFramework").get()
+        val deps = createFrameworkTask.taskDependencies.getDependencies(createFrameworkTask)
+        val outDirs = deps.flatMap {
+            it.outputs.files.files
+        }
+        val plists = outDirs.flatMap {
+            if (it.exists() && it.isDirectory) {
+                Files.walk(it.toPath())
+                    .filter {
+                        it.name == "Info.plist"
+                    }
+                    .collect(toList())
+            } else {
+                listOf()
+            }
+        }
+
+        plists.forEach {
+            logger.warn("Apply XCode 15.3(+) workaround to plist file: $it")
+            exec {
+                commandLine("/usr/libexec/PlistBuddy", "-c", "Set MinimumOSVersion 100.0", it.toFile().absolutePath)
+            }
+        }
+    }
+
+}
+
+afterEvaluate {
+    tasks.named("createXCFramework").dependsOn(tasks.named(fixTask.name))
 }
 
 kotlin {
@@ -58,6 +103,8 @@ kotlin {
         binaries.withType<Framework> {
             export(projects.oidcCore)
             export(projects.oidcTokenstore)
+
+//            freeCompilerArgs += listOf("-Xoverride-konan-properties=minVersion.ios=15.0;minVersionSinceXcode15.ios=15.0")
         }
     }
 }
