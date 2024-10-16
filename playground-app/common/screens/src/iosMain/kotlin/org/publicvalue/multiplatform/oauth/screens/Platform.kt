307b3663a00762ae1fb701bec360df3854d278e8
Julian Kalinowski <julakali@gmail.com>
2024-10-16 07:38:30 +0200

    fix parcelize annotation

--- playground-app/common/screens/src/iosMain/kotlin/org/publicvalue/multiplatform/oauth/screens/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.publicvalue.multiplatform.oauth.screens
-
-actual interface CommonParceler<T>
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.BINARY)
-actual annotation class CommonParcelize actual constructor()
