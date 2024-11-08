73069ba8860df187856d102a53c7685025086c88
Julian Kalinowski <julakali@gmail.com>
2024-11-08 20:02:06 +0100

    feat: update to ktor 3

--- oidc-ktor/src/commonMain/kotlin/org/publicvalue/multiplatform/oidc/ktor/HttpClient+clearTokens.kt
+++ oidc-ktor/src/commonMain/kotlin/org/publicvalue/multiplatform/oidc/ktor/HttpClient+clearTokens.kt
@@ -2,17 +2,17 @@ package org.publicvalue.multiplatform.oidc.ktor
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.authProvider
 import io.ktor.client.plugins.auth.providers.BearerAuthProvider
 import io.ktor.client.plugins.plugin
 
-// https://youtrack.jetbrains.com/issue/KTOR-4759/Auth-BearerAuthProvider-caches-result-of-loadToken-until-process-death
-// Force the Auth plugin to invoke the `loadTokens` block again on the next client request.
+/**
+ * Force the Auth plugin to invoke the `loadTokens` block again on the next client request.
+ *
+ * @see https://youtrack.jetbrains.com/issue/KTOR-4759/Auth-BearerAuthProvider-caches-result-of-loadToken-until-process-death
+*/
+
+@Suppress("unused")
 fun HttpClient.clearTokens() {
-    try {
-        plugin(Auth).providers
-            .filterIsInstance<BearerAuthProvider>()
-            .first().clearToken()
-    } catch (e: IllegalStateException) {
-        // No-op; plugin not installed
-    }
+    authProvider<BearerAuthProvider>()?.clearToken()
 }
