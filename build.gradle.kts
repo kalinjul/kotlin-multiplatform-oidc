a23709ff20e31e76f3a00b3e3efba5ad07855657
Julian Kalinowski <julakali@gmail.com>
2025-07-03 14:33:38 +0200

    ci: publish to central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.compose.internal.utils.getLocalProperty
-import java.net.URI
 
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -20,11 +19,10 @@ subprojects {
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(URI("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(URI("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(getLocalProperty("OSSRH_USERNAME") ?: System.getenv("OSSRH_USERNAME"))
             password.set(getLocalProperty("OSSRH_PASSWORD") ?: System.getenv("OSSRH_PASSWORD"))
-            stagingProfileId.set(getLocalProperty("SONATYPE_STAGING_PROFILE_ID") ?: System.getenv("SONATYPE_STAGING_PROFILE_ID"))
         }
     }
 }
