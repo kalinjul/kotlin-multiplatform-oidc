7bbaa9ce0d94e95d21deb597079194c3eac14930
Oleg Kobzev <kobzevjob@gmail.com>
2025-11-05 09:01:46 +0700

    the wasm test fix

--- oidc-crypto/src/wasmJsMain/kotlin/org/publicvalue/multiplatform/oidc/Crypto.wasmJs.kt
+++ oidc-crypto/src/wasmJsMain/kotlin/org/publicvalue/multiplatform/oidc/Crypto.wasmJs.kt
@@ -8,14 +8,16 @@ import org.kotlincrypto.hash.sha2.SHA256
 /**
  * this delegates to [`crypto.getRandomValues`](https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues)
  */
-public external object Crypto {
-    public fun getRandomValues()
+@JsName("crypto")
+private external object Crypto {
+    @Suppress("unused")
+    fun getRandomValues(uint8Array: Uint8Array)
 }
 
 public actual fun secureRandomBytes(size: Int): ByteArray {
     require(size >= 0) { "count cannot be negative" }
     val uint8Array = Uint8Array(size)
-    Crypto.getRandomValues()
+    Crypto.getRandomValues(uint8Array = uint8Array)
     return ByteArray(size) { uint8Array[it] }
 }
 
