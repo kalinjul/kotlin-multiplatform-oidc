950113d8496845c274485320b4caa1b56daaa977
Julian Kalinowski <julakali@gmail.com>
2025-12-12 22:18:42 +0100

    refactor: move preferences interface to core

--- oidc-preferences/src/commonMain/kotlin/org/publicvalue/multiplatform/oidc/preferences/Preferences.kt
+++
@@ -1,8 +0,0 @@
-package org.publicvalue.multiplatform.oidc.preferences
-
-interface Preferences {
-    suspend fun get(key: String): String?
-    suspend fun put(key: String, value: String)
-    suspend fun remove(key: String)
-    suspend fun clear()
-}
